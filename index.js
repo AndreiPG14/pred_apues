8b2a228c3226c18cd6054ede93dd965af80ee80a
ZCLordP <andreipg2314@gmail.com>
2026-06-29 01:57:01 -0500

    use axios

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 ﻿const express = require("express");
+const axios = require("axios");
 const app = express();
 
 app.use(async (req, res) => {
   try {
     const url = "https://v3.football.api-sports.io" + req.url;
     console.log("Fetching:", url);
-    const r = await fetch(url, {
-      method: "GET",
-      headers: { 
-        "x-apisports-key": "20b03b767c78b8c3ad618f1830a6a6b8",
-        "Content-Type": "application/json"
-      }
+    const r = await axios.get(url, {
+      headers: { "x-apisports-key": "20b03b767c78b8c3ad618f1830a6a6b8" }
     });
-    const data = await r.json();
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "*");
-    res.json(data);
+    res.json(r.data);
   } catch(e) {
     res.status(500).json({ error: e.message });
   }
