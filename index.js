a4bd385aee6611cf402e56fff607ae7e75932132
ZCLordP <andreipg2314@gmail.com>
2026-06-29 02:06:08 -0500

    use env API_KEY

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
 const axios = require("axios");
 const app = express();
 
+const API_KEY = process.env.API_KEY || "20b03b767c78b8c3ad618f1830a6a6b8";
+
 app.use(async (req, res) => {
   try {
     const url = "https://v3.football.api-sports.io" + req.url;
-    console.log("Fetching:", url);
+    console.log("Fetching:", url, "Key:", API_KEY.substring(0,8) + "...");
     const r = await axios.get(url, {
-      headers: { "x-apisports-key": "20b03b767c78b8c3ad618f1830a6a6b8" }
+      headers: { "x-apisports-key": API_KEY }
     });
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.json(r.data);
@@ -16,4 +18,4 @@ app.use(async (req, res) => {
   }
 });
 
-app.listen(process.env.PORT || 8080, () => console.log("Proxy corriendo"));
+app.listen(process.env.PORT || 8080, () => console.log("Proxy corriendo en puerto", process.env.PORT || 8080));
